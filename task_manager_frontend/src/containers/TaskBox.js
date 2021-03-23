c79cfd7a047a93f12f4302f36bdd16227d1d9221
tcrurup <tcrurup@gmail.com>
2021-03-23 15:15:08 -0600

    add container folder and create TaskBox

---
+++ task_manager_frontend/src/containers/TaskBox.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class TaskBox extends Component{
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            tasks: []
+        };
+    }
+    
+    render = () => <div class="taskBox">
+    </div>
+
+    newTask = () => {
+
+    }
+}
+
+export default TaskBox
