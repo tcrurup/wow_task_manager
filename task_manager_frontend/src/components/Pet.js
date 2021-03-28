b17b93928d0cfb26a1e102f7cef03aa81e789bbf
tcrurup <tcrurup@gmail.com>
2021-03-27 22:43:41 -0600

    create PetTeam and Pet classes

---
+++ task_manager_frontend/src/components/Pet.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Pet extends Component{
+
+    constructor(props){
+        super(props)
+    }
+    
+}
+
+export default Pets
---
+++ task_manager_frontend/src/containers/PetTeam.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class PetTeam extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+
+}
+
+export default PetTeam
